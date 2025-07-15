bfe4106fbf042b647f4e63362295beaa1ac305e9
rahulgope45 <rahuleile456@gmail.com>
2025-07-16 03:18:34 +0530

    REsume update

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -12,20 +12,14 @@ function Resume() {
     >
       <h1 className="text-3xl md:text-4xl font-bold text-white mb-6">Resume</h1>
       <div className="w-full max-w-4xl bg-white/10 backdrop-blur-md rounded-lg shadow-lg p-4">
-        <object
-          data="/resume.pdf"
-          type="application/pdf"
-          width="100%"
-          height="500px"
-        >
-          <p className="text-white">
-            Your browser does not support viewing PDFs. 
-            <a href="/resume.pdf" className="text-blue-400 underline">Download the PDF</a> instead.
-          </p>
-        </object>
+          <iframe
+           src ="https://docs.google.com/document/d/1EkTaAqXuaZj-snXyS1DWSi6luFIbzo1HGz0FwG25954/edit?usp=drive_link"
+           style={{ width: '100%', height: '500px' }}
+           frameBorder="0"
+          ></iframe>
           <div className="flex justify-center mt-4">
             <a
-            href="/resume.pdf"
+            href="/resume2.docx"
             download
             className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
             >
