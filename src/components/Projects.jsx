7e9879a9fff73d2ba2aee1ba3e15c5f2bd0a9308
rahulgope45 <rahuleile456@gmail.com>
2025-07-15 19:24:51 +0530

    Animations added 2

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,7 +17,7 @@ function Projects() {
     initial={{ opacity: 0, y: 30 }}
   whileInView={{ opacity: 1, y: 0 }}
   transition={{ duration: 0.6 }}
-  viewport={{ once: false, amount: 0.5 }}
+  viewport={{ once: true }}
     >
      
           <div className="w-full text-center my-10"
