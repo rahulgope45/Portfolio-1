ea29ce932e43542f78924f494dcf72fb3f789ec9
rahulgope45 <rahuleile456@gmail.com>
2025-09-23 23:47:51 +0530

    Updated skills and thier colors

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,5 @@
 import React,{useRef,useState} from 'react'
-import { SiReact, SiTailwindcss, SiJavascript, SiCss3 ,  SiPython, SiFirebase, SiGit, SiGithub,  SiHtml5 , SiDart, SiFlutter } from "react-icons/si";
+import { SiReact, SiTailwindcss, SiJavascript, SiCss3 ,  SiPython, SiFirebase, SiGit, SiGithub,  SiHtml5 , SiDart, SiFlutter, SiNodedotjs, SiExpress, SiCloudinary, SiSocketdotio } from "react-icons/si";
 import { FaJava } from "react-icons/fa";
 import SpotlightCard from './Spotlightcard';
 import { motion } from "framer-motion";
@@ -161,6 +161,46 @@ function Skills() {
       <div className="flex flex-col items-center">
       <SiFlutter  className="text-sky-400 text-4xl mb-2" />
       <p className="text-white font-medium">Flutter</p>
+    </div>
+
+    </SpotlightCard>
+    <SpotlightCard
+     spotlightColor="rgba(187, 0, 255, 0.4)"
+     className="backdrop-blur-md bg-white/10 border border-white/20 rounded-xl p-6 shadow-md hover:scale-105 transition-transform duration-200"
+    >
+      <div className="flex flex-col items-center">
+      <SiNodedotjs  className="text-green-500 text-4xl mb-2"/>
+      <p className="text-white font-medium">Node Js</p>
+    </div>
+
+    </SpotlightCard>
+    <SpotlightCard
+     spotlightColor="rgba(187, 0, 255, 0.4)"
+     className="backdrop-blur-md bg-white/10 border border-white/20 rounded-xl p-6 shadow-md hover:scale-105 transition-transform duration-200"
+    >
+      <div className="flex flex-col items-center">
+      <SiExpress  className="text-gray-300 text-4xl mb-2"/>
+      <p className="text-white font-medium">Express Js</p>
+    </div>
+
+    </SpotlightCard>
+    <SpotlightCard
+     spotlightColor="rgba(187, 0, 255, 0.4)"
+     className="backdrop-blur-md bg-white/10 border border-white/20 rounded-xl p-6 shadow-md hover:scale-105 transition-transform duration-200"
+    >
+      <div className="flex flex-col items-center">
+      <SiCloudinary  className="text-indigo-400 text-4xl mb-2" />
+      <p className="text-white font-medium">Cloudinary</p>
+    </div>
+
+    </SpotlightCard>
+    <SpotlightCard
+     spotlightColor="rgba(187, 0, 255, 0.4)"
+     className="backdrop-blur-md bg-white/10 border border-white/20 rounded-xl p-6 shadow-md hover:scale-105 transition-transform duration-200"
+    >
+      <div className="flex flex-col items-center">
+      <SiSocketdotio  className="text-pink-400 text-4xl mb-2" />
+      <p className="text-white font-medium">WebSocket</p>
     </div>
 
     </SpotlightCard>
