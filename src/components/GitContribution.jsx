06bf90b72939d3ca6c8098ebc1cb2c7249010e5d
rahulgope45 <rahuleile456@gmail.com>
2025-09-30 01:07:49 +0530

    Git hub calender added styles are done 2

--- src/components/GitContribution.jsx
+++ src/components/GitContribution.jsx
@@ -6,7 +6,8 @@ function GitContributions({rahulgope45}) {
 
   const explicitTheme ={
      light: ['#ebedf0', '#9be9a8', '#40c463', '#30a14e', '#216e39'],
-    dark: ['#161b22', '#0e4429', '#006d32', '#26a34c', '#48e063']
+    dark: ['#161b22', '#0e4429', '#006d32', '#26a34c', '#48e063'],
+    text: '#ffffffff',
 
 
   }
@@ -19,24 +20,30 @@ function GitContributions({rahulgope45}) {
 
 
   return (
-    <div className="mx-auto my-8 p-6 bg-white dark:bg-gray-800 rounded-xl shadow-lg border border-gray-200 dark:border-gray-700 max-w-4xl overflow-x-auto">
+    <div className="mx-auto my-8 p-6 bg-white dark:bg-gray-800 rounded-xl shadow-lg border border-gray-200 dark:border-gray-700 max-w-4xl overflow-x-auto text-white">
        <h2 className="text-2xl font-bold mb-6 text-gray-900 dark:text-gray-100 border-b pb-2 border-gray-300 dark:border-gray-700">
         My GitHub Activity
       </h2>
       <GitHubCalendar
+      
       username='rahulgope45'
       fontSize={14}
         blockSize={12}
         blockMargin={4}
         showWeekdayLabels
         hideTotalCount={false}
+        
+        
+
       
        theme={explicitTheme} 
        loading={loadingfallback()}
        
        labels={{
-            totalCount: '{{count}} contributions in the last year',
+            totalCount: '{{count}} contributions in the last year'
+            
         }}
+        
 
       
       />
